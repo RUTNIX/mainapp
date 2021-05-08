258c1cb9dc1ac61b7b48c5aa200702e370db6c2a
Али Бейшенбеков <alibejsenbekov@MacBook-Air-Ali.local>
2021-05-08 15:28:45 +0600

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alibejsenbekov/Desktop/fourth/clnew/Marketplace/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
